6b42db06a67234370fec768286cfabeadacb6ede
Ngoc Phat <ngocphat12092015@gmail.com>
2022-05-19 11:59:29 +0700

    frontend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/forgot', function(req, res, next) {
+  res.render('forgot');
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('products');
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+  res.render('shopping-cart');
 });
 
 module.exports = router;
